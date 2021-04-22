a70db6d3b7bda6fc3f2e35bd4c324bb661b90c2e
Alena <a.a.lebedenko056@mail.ru>
2021-04-22 23:19:00 +0500

    бот

--- bot.py
+++ bot.py
@@ -3,13 +3,12 @@ from discord.ext import commands
 
 import os
 
-from music_code import music_bot
-
+from music_bot import music_bot
 
 bot = commands.Bot(command_prefix='/')
 
-#Регистрируем класс у бота
+# Регистрируем класс у бота
 bot.add_cog(music_bot(bot))
 
-#Запускаем с моего токена
+# Запускаем с моего токена
 bot.run('ODM0ODE5MTcwMTg4Nzg3NzEz.YIGb8A.mauKlPkkLqr9zXJGrL30QLtyzHw')
